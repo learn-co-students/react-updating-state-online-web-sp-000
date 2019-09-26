bd88396f6f7bf9915c599e8a2418278db620245a
Diana Badas <diana.badas@gmail.com>
2019-09-26 16:37:11 +0100

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -14,7 +14,7 @@ class ClickityClick extends React.Component {
     handleClick = () => {
         //Update our state here
         this.setState({
-            hasBeeClicked: true
+            hasBeenClicked: true
         }, () => console.log(this.state.hasBeenClicked))
     }
    
