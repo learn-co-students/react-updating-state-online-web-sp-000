d07e4986224cfcf97142f609c87ff747dfa03840
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-09-30 18:38:15 +0100

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,33 @@
-// Code ClickityClick Component Here
+import React from 'react';
+ 
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      toggled: false
+    };
+  }
+ 
+  handleClick = () => {
+    // Update our state here...
+    // State changes, however instant they might appear, happen asynchronously. If we want to access our new state after it has been updated, we can optionally add a callback as a second argument to the this.setState() function. This callback will fire once the state has been updated, ensuring that this.state is now the new, shiny updated state.
+    // when handleClick is called, setState will update the state so that toggle is reversed
+    this.setState(previousState => {
+        return {
+            toggled: !previousState.toggled
+        }
+    })
+  };
+ 
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+      </div>
+    );
+  }
+}
+ 
+export default ClickityClick;
