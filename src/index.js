0f24e80a4adf4b37ba91eebc5d69e86350a2f2d4
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-24 03:28:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClackity from './components/ClickityClick'
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityClackity/>,
   document.getElementById('root')
 );
